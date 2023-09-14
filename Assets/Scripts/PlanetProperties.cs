01899b67c33e07e0863dff93ceb827d7078222d6
Aryan Methil <aryanmethil@gmail.com>
2023-09-14 15:02:29 -0700

    Planets rings can be sucked
    - Planets randomly spawn in 4 horizontally sectioned areas of 32 (width) x 10 area
    - Player is just a circle which moves horizontally (across the width).
      No movement mechanic for player
    - Player can suck a planet's rings such that:
    	- Planet has to be in a fixed radius of the player at a frame
    	- The planet's rings are not sucked yet.
    	- If both things above are True -> planet is green
    	- Ring is sucked by pressing spacebar
    	- After successfull sucking, rings disappear

---
+++ Assets/Scripts/PlanetProperties.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetProperties : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string prefabName = "Planet";
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
