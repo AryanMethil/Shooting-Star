d3fb85a71285bb9fbe0f07b093f51ce52747eae8
Aryan Methil <aryanmethil@gmail.com>
2023-09-16 16:50:24 -0700

    Game Over and Restart UI added
    - Game terminates when enemy touches good guy and then everything stops
      updating
    - Restart button will reload the game scene

--- Assets/Scripts/MovingBall.cs
+++ Assets/Scripts/MovingBall.cs
@@ -19,30 +19,33 @@ public class MovingBall : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.localPosition+=(displacement*speed*Time.deltaTime);
+        bool gameOverFlag = GameObject.FindGameObjectWithTag("GameOverFlag").GetComponent<GameOverFlagScript>().gameOverFlag;
+        if(!gameOverFlag){
+            transform.localPosition+=(displacement*speed*Time.deltaTime);
         
-        GameObject[] allGameObjects = GameObject.FindObjectsOfType<GameObject>();
-        foreach(GameObject obj in allGameObjects){
-            PlanetProperties customProperties = obj.GetComponent<PlanetProperties>();
-            if (customProperties != null)
-            {
-                float distance = Vector3.Distance(transform.localPosition, obj.transform.position);
-                if(distance<=radiusOfView && obj.transform.childCount==2){
-                    obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = new Color(0,255,0);
-                    if(Input.GetKeyDown("space")){
-                        Object.Destroy(obj.transform.GetChild(1).gameObject);
+            GameObject[] allGameObjects = GameObject.FindObjectsOfType<GameObject>();
+            foreach(GameObject obj in allGameObjects){
+                PlanetProperties customProperties = obj.GetComponent<PlanetProperties>();
+                if (customProperties != null)
+                {
+                    float distance = Vector3.Distance(transform.localPosition, obj.transform.position);
+                    if(distance<=radiusOfView && obj.transform.childCount==2){
+                        obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = new Color(0,255,0);
+                        if(Input.GetKeyDown("space")){
+                            Object.Destroy(obj.transform.GetChild(1).gameObject);
+                            obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = new Color(255,255,255);
+                            int curr_ammo = int.Parse(text.GetComponent<UnityEngine.UI.Text>().text.Substring(6));
+                            int updated_ammo = curr_ammo+10;
+                            text.GetComponent<UnityEngine.UI.Text>().text = "Ammo: "+updated_ammo.ToString();
+                            break;
+                        }
+                    }
+                    else{
                         obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = new Color(255,255,255);
-                        int curr_ammo = int.Parse(text.GetComponent<UnityEngine.UI.Text>().text.Substring(6));
-                        int updated_ammo = curr_ammo+10;
-                        text.GetComponent<UnityEngine.UI.Text>().text = "Ammo: "+updated_ammo.ToString();
-                        break;
                     }
                 }
-                else{
-                    obj.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = new Color(255,255,255);
-                }
-            }
             
+            }
         }
     }
 }
---
+++ Assets/Scripts/MovingEnemy.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject goodGuy;
+    public Vector3 offset;
+    public float speed = 5.0f;
+    public Vector3 displacement = new Vector3(0.5f,0,0);
+    public float slowedDuration = 20.0f;
+    public float slowedTimer = 0.0f;
+    public bool isSlowed = false;
+    public TMPro.TextMeshProUGUI gameOverText;
+    public Button restartButton;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        bool gameOverFlag = GameObject.FindGameObjectWithTag("GameOverFlag").GetComponent<GameOverFlagScript>().gameOverFlag;
+        if(!gameOverFlag){
+            if(isSlowed==true){
+                slowedTimer+=Time.deltaTime;
+                if(slowedTimer>=slowedDuration){
+                    isSlowed=false;
+                    slowedTimer=0.0f;
+                    Rigidbody2D rb = GetComponent<Rigidbody2D>();
+                    rb.constraints = RigidbodyConstraints2D.None;
+                }
+            
+            }
+            else{
+                Vector3 direction = (goodGuy.transform.position - transform.localPosition).normalized;
+                transform.localPosition+=(direction*speed*Time.deltaTime);
+            }
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        
+        if(col.gameObject.name=="Bullet(Clone)"){
+            isSlowed=true;
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+        }
+        else{
+            GameObject.FindGameObjectWithTag("GameOverFlag").GetComponent<GameOverFlagScript>().gameOverFlag = true;
+            GameOver();
+        }
+    }
+
+    void GameOver(){
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+}
